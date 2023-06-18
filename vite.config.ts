33531b3a6fde02533bba880601fdf360c374f714
Karol Banat <karol.banat@gmail.com>
2023-06-18 21:16:03 +0200

    Update homepage

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [svelte()],
-	base: '/fem-svelte-single-page-developer-portfolio/',
+	base: '/fem-svelte-sp-dev-portfolio/',
 });
